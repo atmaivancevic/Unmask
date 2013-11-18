6957b15a799fb0483f8800651a88b260e1452e35
atma <atma@ubuntu.(none)>
2013-11-19 02:49:36 +1030

    change label value to identifier

--- main.go
+++ main.go
@@ -8,29 +8,29 @@ import (
 
 const genomeDir = "/scratch/atmaGenomes/"
 
-// this runs unmask on the file [prefix][value].[extension] within the specific genome directory genomeDir/genomeName
+// this runs unmask on the file [prefix][identifier].[extension] within the specific genome directory genomeDir/genomeName
 // prefix = chr/scaffold (for genomes where the files are seperated into chr/scaffolds);
 // or scientific name (for genomes with a single genome files)
-func unmaskChr(genomeName, prefix, extension, value string) {
+func unmaskChr(genomeName, prefix, extension, identifier string) {
 
 	os.Chdir(genomeDir + genomeName)
 
-	inputFileName := prefix + value + "." + extension
-	outputFileName := prefix + value + "_unmasked." + extension
+	inputFileName := prefix + identifier + "." + extension
+	outputFileName := prefix + identifier + "_unmasked." + extension
 
 	// if the unmasked file does not exist, create it
 	if _, err := os.Stat(outputFileName); os.IsNotExist(err) {
 		unmask(inputFileName, outputFileName)
-		fmt.Println("Processed: " + genomeName + "/" + prefix + value)
+		fmt.Println("Processed: " + genomeName + "/" + prefix + identifier)
 	} else {
-		fmt.Println(prefix + value + " already unmasked; skipping.")
+		fmt.Println(prefix + identifier + " already unmasked; skipping.")
 	}
 
 }
 
 // run unmask on a range of values
-func unmaskChrRange(genomeName, prefix, extension string, startValue, endValue int) {
-	for i := startValue; i <= endValue; i++ {
+func unmaskChrRange(genomeName, prefix, extension string, startIdentifier, endIdentifier int) {
+	for i := startIdentifier; i <= endIdentifier; i++ {
 		unmaskChr(genomeName, prefix, extension, strconv.Itoa(i))
 	}
 }
