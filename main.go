e3bcfbaa733de82289fa28c3579dd5643f499bfd
Atma <atma.ivancevic@adelaide.edu.au>
2015-03-25 17:13:00 +1030

    updated genome dir

--- main.go
+++ main.go
@@ -5,7 +5,7 @@ import (
 	"os"
 )
 
-const genomeDir = "/home/atma/Vertebrates/"
+const genomeDir = "/data01/Genomes/Other/"
 
 // this runs unmask on the file [prefix][identifier].[extension] within the specific genome directory [genomeDir]/[genomeName]
 
